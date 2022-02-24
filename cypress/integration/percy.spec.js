16bfb08f7bc4deec45c2e9000fc9310cc7f2c2f2
Cythilya Tang <cythilya@gmail.com>
2022-02-24 22:50:10 +0800

    feat: update visit page for visual testing

--- cypress/integration/percy.spec.js
+++ cypress/integration/percy.spec.js
@@ -1,6 +1,6 @@
 describe('Visual Testing with Percy and Cypress', () => {
     it('should take percy snapshot', () => {
-        cy.visit('https://cythilya.github.io/2021/07/27/ten-snacks-for-better-health/')
+        cy.visit('https://cythilya.github.io/about/')
         cy.wait(1000)
         cy.percySnapshot()
     })
